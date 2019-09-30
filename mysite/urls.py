baf72d8f9389802ef991e67aad3d45889720cc13
fenshitianyue <abzanda@163.com>
2019-09-30 18:59:40 +0800

    update url

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from . import view
+from . import testdb
 # from django.contrib import admin
 #
 # urlpatterns = [
 #     url(r'^admin/', admin.site.urls),
 # ]
 urlpatterns = [
-    url(r'^$', view.hello),
+    url(r'^hello/$', view.hello),
+    url(r'^testdb/$', testdb.testdb),
 ]
 
