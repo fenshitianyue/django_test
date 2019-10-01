935c15ef21f80fc517d5c5d56ce8fcf0f25cbe36
fenshitianyue <abzanda@163.com>
2019-10-01 23:42:38 +0800

    update code

--- mysite/search2.py
+++ mysite/search2.py
@@ -2,7 +2,7 @@
 
 # from django.shortcuts import render_to_response
 from django.shortcuts import render
-from django.core.context_processors import csrf
+# from django.core.context_processors import csrf
 
 import sys
 reload(sys)
@@ -11,7 +11,7 @@ sys.setdefaultencoding('utf-8')
 # 接收POST请求数据
 def search_post(request):
     ctx = {}
-    ctx.update(csrf(request))
+    # ctx.update(csrf(request))
     if request.method == "POST":  # 不建议使用 if request.POST: 来判断是否使用POST方法
         ctx['rlt'] = request.POST['q']
     return render(request, "post.html", ctx)
